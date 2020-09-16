54e1b000f1f9dfc9964d1783759bad2ff2141403
Adrian-Revilla <revilla.adrian02@gmail.com>
2020-09-16 12:22:11 -0300

    .

---
+++ docs/precache-manifest.299cc001606f525a5e437769c4663b1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60b86e160afc8d3f32986201a3d4298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd5e266e2dc6c0009c8",
+    "url": "/static/css/main.58b58455.chunk.css"
+  },
+  {
+    "revision": "1baa89484df53fb4b7ee",
+    "url": "/static/js/2.398034d5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.398034d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd5e266e2dc6c0009c8",
+    "url": "/static/js/main.3b042dcf.chunk.js"
+  },
+  {
+    "revision": "4e9b275e15f9b709d305",
+    "url": "/static/js/runtime-main.2659dcd9.js"
+  },
+  {
+    "revision": "db38ff51fc7f784c2ed5d1fb695d34d4",
+    "url": "/static/media/borradorico.db38ff51.ico"
+  },
+  {
+    "revision": "1f27b1c5612375a18aa996434379e18f",
+    "url": "/static/media/pincel.1f27b1c5.ico"
+  }
+]);
